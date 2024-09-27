a5d7a374a643389e70b4efaddefce7fd788dfa9b
rumaiz013414 <166476270+rumaiz013414@users.noreply.github.com>
2024-09-27 20:17:40 +0530

    implemented structure

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import '../services/api_service.dart';
+import 'cart_screen.dart';
 import 'login_screen.dart';
+import 'home_content_screen.dart';
+import 'profile_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -9,6 +12,19 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final ApiService _apiService = ApiService();
+  int _selectedIndex = 0;
+
+  static List<Widget> _screens = <Widget>[
+    HomeContentScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   void _logout() async {
     await _apiService.logout();
@@ -31,8 +47,25 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Center(
-        child: Text('Welcome to the Home Screen!'),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_checkout),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex, // This will highlight the selected item
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped, // Update the index on tap
       ),
     );
   }
